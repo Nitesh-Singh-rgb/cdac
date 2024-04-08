605347dd8696acb1a98ddfc9d92cb6f09c6d665b
Nitesh-Singh-rgb <63577885+Nitesh-Singh-rgb@users.noreply.github.com>
2024-04-08 18:11:00 +0530

    Add files via upload

--- Java/Language-Runtime/GenericMethodTest/demo/Program3.java
+++ Java/Language-Runtime/GenericMethodTest/demo/Program3.java
@@ -6,6 +6,12 @@ class Program {
         return second;
     }
 
+    private static <T extends Comparable<T>> T select(T first, T second) {
+        if(first.compareTo(second) > 0)
+            return first;
+        return second;
+    }
+
     public static void main(String[] args) {
         if(args.length > 0){
             int s = Integer.parseInt(args[0]);
@@ -19,6 +25,13 @@ class Program {
             System.out.printf("Selected double value = %s%n", sd);
             //double ssd = select(s, "Jack", 2.3);
             //System.out.printf("Selected int value = %s%n", ssd);
+        }else{
+            String ss = select("Monday", "Tuesday");
+            System.out.printf("Selected String value = %s%n", ss);
+            double sd = select(43.5, 32.6);
+            System.out.printf("Selected double value = %s%n", sd);  
+            Interval si = select(new Interval(3, 45), new Interval(2, 30));
+            System.out.printf("Selected Interval value = %s%n", si);
         }
     }
 }
