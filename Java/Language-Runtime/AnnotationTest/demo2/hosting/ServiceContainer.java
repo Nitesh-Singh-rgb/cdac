64e7e862829a0df9ef97c11f232cbc86f4739375
Nitesh-Singh-rgb <nsingh2931@gmail.com>
2024-04-23 12:02:33 +0530

    latest

--- Java/Language-Runtime/AnnotationTest/demo2/hosting/ServiceContainer.java
+++ Java/Language-Runtime/AnnotationTest/demo2/hosting/ServiceContainer.java
@@ -8,17 +8,23 @@ public class ServiceContainer {
     
     @SuppressWarnings("unchecked")
     public static <I, J extends I> I getService(Class<I> iface, Class<J> impl) throws Exception {
-        System.out.printf("Configuring environment for %s service.%n", impl.getSimpleName());
-        if(impl.isAnnotationPresent(OnePerCall.class)){
-            var proxyHandler = new InvocationHandler() {
-                public Object invoke(Object proxyObject, Method targetMethod, Object[] args) throws Exception {
-                    Object targetObject = impl.getConstructor().newInstance();
-                    return targetMethod.invoke(targetObject, args);
-                }
-            };
-            return  (I)Proxy.newProxyInstance(impl.getClassLoader(), impl.getInterfaces(), proxyHandler);
-        }
+        System.out.printf("Configuring environment for %s service...%n", impl.getSimpleName());
+        if(impl.isAnnotationPresent(OnePerCall.class))
+            return (I) Proxy.newProxyInstance(impl.getClassLoader(), impl.getInterfaces(), new OnePerCallProxyHandler(impl));
         return impl.getConstructor().newInstance();
     }
 
+    static class OnePerCallProxyHandler implements InvocationHandler {
+
+        private Class<?> targetClass;
+
+        OnePerCallProxyHandler(Class<?> targetClass) {
+            this.targetClass = targetClass;
+        }
+
+        public Object invoke(Object proxyObject, Method targetMethod, Object[] args) throws Exception {
+            Object targetObject = targetClass.getConstructor().newInstance();
+            return targetMethod.invoke(targetObject, args);
+        }
+    }
 }
